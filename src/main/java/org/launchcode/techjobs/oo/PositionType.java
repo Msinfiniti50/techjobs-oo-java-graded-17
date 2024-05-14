56bfada4a904188c72d503644e6d2d5411d38fde
Msinfiniti50 <hjones0973@gmail.com>
2024-05-13 20:05:31 -0500

    done

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -2,20 +2,20 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class PositionType extends Job {
+public class PositionType extends Job {// inherits same behavior/methods from parent class
 
-    private final int id;
+    private final int id;// generating a unique ID for each object and add 1 for the next ID
     private static int nextId = 1;
     private String value;
 
-    public PositionType() {
+    public PositionType() { //constructor is assigning value to new ID field of object
         id = nextId;
         nextId++;
     }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        this();// constructor with one parameter, This assigns new unique ID to every new PTO
+        this.value = value; //sets the value
     }
 @Override
 public String toString() {
@@ -34,21 +34,21 @@ public String toString() {
 
     public int getId() {
         return id;
-    }
+    }// returns value of ID
 
     public String getValue() {
         return value;
-    }
+    } //encapsulation that retrieves the value data Field
 
     public void setValue(String value) {
         this.value = value;
-    }
+    }// allows you to modify the v.a of instances of Class where method is
 
 
 @Override
 public int hashCode() {
     return Objects.hash(id);
-}
+}// stores collection with the unique identifiers for easy searching
 @Override
 public boolean equals(Object o) {
     if (this == o) return true;
